23d2ad7de67066b85b4600cddba36fdd005e3053
zzkeln <zhengzhibin90@gmail.com>
2019-07-20 10:47:47 +0800

    Update slice.go

--- internal/stringy/slice.go
+++ internal/stringy/slice.go
@@ -1,5 +1,8 @@
 package stringy
 
+/*提供一些字符串切片的方法，例如2个字符串切片的并集，对字符串切片去重，添加一个字符串到切片中*/
+
+//将字符串a添加到字符串切片s中（只有不存在的情况下），返回新的切片
 func Add(s []string, a string) []string {
 	for _, existing := range s {
 		if a == existing {
@@ -10,6 +13,7 @@ func Add(s []string, a string) []string {
 
 }
 
+//将a的元素并入s中，返回s。这里会改变参数s，而且遍历s未必高效，也可以试试map来check-exist
 func Union(s []string, a []string) []string {
 	for _, entry := range a {
 		found := false
@@ -26,6 +30,7 @@ func Union(s []string, a []string) []string {
 	return s
 }
 
+//对s进行去重，返回去重后的结果，这里并不改变参数s
 func Uniq(s []string) (r []string) {
 	for _, entry := range s {
 		found := false
