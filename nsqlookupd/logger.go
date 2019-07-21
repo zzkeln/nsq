d6983924d0d56928e0ab233dd01b95ddb51946ef
zzkeln <zhengzhibin90@gmail.com>
2019-07-21 18:14:40 +0800

    Update logger.go

--- nsqlookupd/logger.go
+++ nsqlookupd/logger.go
@@ -4,8 +4,12 @@ import (
 	"github.com/nsqio/nsq/internal/lg"
 )
 
+/*实现了nsqlookupd包里的logf接口，使用options的日志等级作为配置日志等级*/
+
+//声明了Logger的类型
 type Logger lg.Logger
 
+//定义了5种log类型
 const (
 	LOG_DEBUG = lg.DEBUG
 	LOG_INFO  = lg.INFO
@@ -14,6 +18,8 @@ const (
 	LOG_FATAL = lg.FATAL
 )
 
+//实现logf接口
 func (n *NSQLookupd) logf(level lg.LogLevel, f string, args ...interface{}) {
+	//配置日志等级使用options里的
 	lg.Logf(n.opts.Logger, n.opts.LogLevel, level, f, args...)
 }
