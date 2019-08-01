bc9e124a59d436c906d2116e0b748c4f08912fcd
zzkeln <zhengzhibin90@gmail.com>
2019-08-01 10:31:34 +0800

    Update options.go

--- nsqlookupd/options.go
+++ nsqlookupd/options.go
@@ -9,18 +9,19 @@ import (
 )
 
 type Options struct {
-	LogLevel  lg.LogLevel `flag:"log-level"`
-	LogPrefix string      `flag:"log-prefix"`
+	LogLevel  lg.LogLevel `flag:"log-level"`           //日志等级
+	LogPrefix string      `flag:"log-prefix"`          //日志前缀
 	Logger    Logger
 
-	TCPAddress       string `flag:"tcp-address"`
-	HTTPAddress      string `flag:"http-address"`
-	BroadcastAddress string `flag:"broadcast-address"`
+	TCPAddress       string `flag:"tcp-address"`       //监听的tcp地址
+	HTTPAddress      string `flag:"http-address"`      //监听的http地址
+	BroadcastAddress string `flag:"broadcast-address"` //广播地址
 
-	InactiveProducerTimeout time.Duration `flag:"inactive-producer-timeout"`
+	InactiveProducerTimeout time.Duration `flag:"inactive-producer-timeout"`  //生产者超时时间，默认300s
 	TombstoneLifetime       time.Duration `flag:"tombstone-lifetime"`
 }
 
+//返回nsqlookupd的配置选项
 func NewOptions() *Options {
 	hostname, err := os.Hostname()
 	if err != nil {
@@ -28,13 +29,13 @@ func NewOptions() *Options {
 	}
 
 	return &Options{
-		LogPrefix:        "[nsqlookupd] ",
-		LogLevel:         lg.INFO,
+		LogPrefix:        "[nsqlookupd] ",  //日志前缀
+		LogLevel:         lg.INFO,          //日志等级是info
 		TCPAddress:       "0.0.0.0:4160",
 		HTTPAddress:      "0.0.0.0:4161",
 		BroadcastAddress: hostname,
 
-		InactiveProducerTimeout: 300 * time.Second,
+		InactiveProducerTimeout: 300 * time.Second, //300s超时
 		TombstoneLifetime:       45 * time.Second,
 	}
 }
