e4a9452b358fa7e3f1186ffb667d56ecdf2f5bdb
zzkeln <zhengzhibin90@gmail.com>
2019-08-03 17:47:39 +0800

    Update spread_writer.go

--- internal/writers/spread_writer.go
+++ internal/writers/spread_writer.go
@@ -5,13 +5,15 @@ import (
 	"time"
 )
 
+//封装了一个writer
 type SpreadWriter struct {
-	w        io.Writer
-	interval time.Duration
-	buf      [][]byte
-	exitCh   chan int
+	w        io.Writer       //输出流
+	interval time.Duration	 //这个输出流需要多少时间来刷新完缓存到目标地
+	buf      [][]byte        //内存缓存
+	exitCh   chan int        //停止刷新标志
 }
 
+//返回一个新的输出流
 func NewSpreadWriter(w io.Writer, interval time.Duration, exitCh chan int) *SpreadWriter {
 	return &SpreadWriter{
 		w:        w,
@@ -21,23 +23,30 @@ func NewSpreadWriter(w io.Writer, interval time.Duration, exitCh chan int) *Spre
 	}
 }
 
+//写p字节到缓存中
 func (s *SpreadWriter) Write(p []byte) (int, error) {
+	//将p深拷贝一份，并添加到buf中
 	b := make([]byte, len(p))
 	copy(b, p)
 	s.buf = append(s.buf, b)
 	return len(p), nil
 }
 
+//将缓存全部刷新到目的地，interval控制刷新的频率
 func (s *SpreadWriter) Flush() {
+	//每次刷新完需要interval时间，共要刷新len(s.buf)个内存块，那么每个内存块刷新完等待interval/len时间
 	sleep := s.interval / time.Duration(len(s.buf))
+	//创建一个定时器，这个定时器每到sleep秒后就醒来一次
 	ticker := time.NewTicker(sleep)
+	//遍历所有内存块
 	for _, b := range s.buf {
-		s.w.Write(b)
+		s.w.Write(b) //刷新缓存到输出流
+		//等待sleep秒，或者停止标志被写入
 		select {
 		case <-ticker.C:
 		case <-s.exitCh: // skip sleeps finish writes
 		}
 	}
-	ticker.Stop()
-	s.buf = s.buf[:0]
+	ticker.Stop() //停止定时器
+	s.buf = s.buf[:0] //清空缓存
 }
