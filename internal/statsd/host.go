44b532f6f4a8ca59bde30c259a8fe8b2c532fc78
zzkeln <zhengzhibin90@gmail.com>
2019-08-03 20:44:39 +0800

    Update host.go

--- internal/statsd/host.go
+++ internal/statsd/host.go
@@ -4,6 +4,9 @@ import (
 	"strings"
 )
 
+//将ip:port中的.和:都换成_
+
+//h主要是127.0.0.1:8888-->127_0_0_1_8888
 func HostKey(h string) string {
 	return strings.Replace(strings.Replace(h, ".", "_", -1), ":", "_", -1)
 }
